0eb2a53ff1232218e8aea15cbba24becb63dfb48
Christina Trochidou <xristinatrox@yahoo.gr>
2026-01-09 12:18:01 +0200

    Tranferred hardcoded books from script.js to index.js, Updated frontend to fetch book data from backend, remove unnecessary code from script.js and updated loadbooks to dynamically create book cards from backend response.

---
+++ index.js
@@ -0,0 +1,309 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+
+
+const books = [
+  {
+  title:"1984", 
+  author:"George Orwell", 
+  pages:328, 
+  read:true, 
+  imageURL:"https://cdn.waterstones.com/bookjackets/large/9780/1410/9780141036144.jpg",
+  id:crypto.randomUUID(),
+},
+{
+  title:"Burial Rights",
+  author:"Hannah Kent",
+  pages:420,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/81yBp-oayWL._AC_UF894,1000_QL80_.jpg",
+  id: crypto.randomUUID(),
+},
+{
+  title:"The Shadow of the Wind",
+  author:"Carlos Ruiz Zafón",
+  pages:544,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/913DdP7RflL._AC_UF1000,1000_QL80_.jpg",
+  id: crypto.randomUUID(),
+},
+{
+  title:"Chess Story",
+  author:"Stefan Zweig",
+  pages:112,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/71I6zQJwPnL._AC_UF1000,1000_QL80_.jpg",
+  id: crypto.randomUUID(),
+},
+{
+  title:"Brave New World",
+  author:"Aldous Huxley",
+  pages:288,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1575509280i/5129.jpg",
+ id:crypto.randomUUID(),
+},
+{
+  title:"Lord of Flies",
+  author:"William Golding",
+  pages:240,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/81xVnX8OodL._AC_UF1000,1000_QL80_.jpg",
+  id: crypto.randomUUID()
+},
+{
+  title:"His Dark Materials Trilogy",
+  author:"Philip Pullman",
+  pages:1088,
+  read:true,
+  imageURL:"https://prodimage.images-bn.com/pimages/9780307957832_p0_v2_s600x595.jpg",
+  id:crypto.randomUUID()
+},
+{
+  title:"Frankenstein",
+  author:"Mary Shelley",
+  pages:240,
+  read:true,
+  imageURL:"https://www.magicmurals.com/media/catalog/product/cache/af1e2a1566fa2dbb552605e8822354b7/a/d/adg-0000001045_1.jpg",
+  id:crypto.randomUUID()
+},
+{
+title:"Confusion",
+author:"Stefan Zweig",
+pages:176,
+read:true,
+imageURL:"https://m.media-amazon.com/images/I/71+v5U30orL._AC_UF1000,1000_QL80_.jpg",
+id:crypto.randomUUID()
+},
+{
+  title:"When Nietzsche Wept",
+  author:"Irvin D. Yalom",
+  pages:310,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/81z5b4ZHKoL._AC_UF1000,1000_QL80_.jpg",
+  id:crypto.randomUUID()
+},
+{
+  title:"Girl, Woman, Other",
+  author:"Bernardine Evaristo",
+  pages:464,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/71TpVXA19ZL._AC_UF1000,1000_QL80_.jpg",
+  id:crypto.randomUUID(),
+},
+{
+  title:"When the Crawdads Sing",
+  author:"Delia Owens",
+  pages:384,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/81e+mSqZvnL._AC_UF894,1000_QL80_.jpg",
+  id:crypto.randomUUID()
+},
+{
+  title:"The Secret History",
+  author:"Donna Tart",
+  pages:559,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/81YhQfeiynL.jpg",
+  id:crypto.randomUUID()
+},
+{
+  title:"Love's Executioner and Other Tales of Psychotherapy",
+  author:"Irvin D. Yalom",
+  pages:304,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/81ciP8gyJTL._AC_UF1000,1000_QL80_.jpg",
+  id:crypto.randomUUID()
+},
+{
+  title:"Animal Farm",
+  author:"George Orwell",
+  pages:112,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/91Lbhwt5RzL.jpg",
+  id:crypto.randomUUID(),
+},
+{
+  title:"Just Above my Head",
+  author:"James Baldwin",
+  pages:584,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/81JUgWD6xWL._AC_UF1000,1000_QL80_.jpg",
+  id:crypto.randomUUID(),
+},
+{
+  title:"Memoirs of a Geisha",
+  author:"Arthur Golden",
+  pages:503,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1409595968i/929.jpg",
+  id:crypto.randomUUID()
+},
+{
+  title:"The Picture of Dorian Gray",
+  author:"Oscar Wilde",
+  pages:272,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1546103428i/5297.jpg",
+  id:crypto.randomUUID()
+},
+{
+  title:"Invisible Women",
+  author:"Caroline Criado Perez",
+  pages:272,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/61OCni--zZL._AC_UF1000,1000_QL80_.jpg",
+  id:crypto.randomUUID(),
+},
+{
+  title:"Desert Flower",
+  author:"Waris Dirie",
+  pages:256,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1439762656i/8745.jpg",
+  id:crypto.randomUUID(),
+},
+{
+  title:"I'll Give you the Sun",
+  author:"Jandy Nelson",
+  pages:371,
+  read:true,
+  imageURL:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1608612077l/20820994.jpg",
+  id:crypto.randomUUID()
+},
+{
+  title:"The Song of Achilles",
+  author:"Madeline Miller",
+  pages:408,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1357177533i/13623848.jpg",
+  id:crypto.randomUUID()
+},
+{
+title:"The Seven Husbands of Evelyn Hugo",
+author:"Taylor Jenkins Reid",
+pages:389,
+read:true,
+imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1664458703i/32620332.jpg",
+id:crypto.randomUUID(),
+},
+{
+title:"The Seven Deaths of Evelyn Hardcastle",
+author:"Stuart Turton",
+pages:432,
+read:true,
+imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1506896221i/36337550.jpg",
+id:crypto.randomUUID(),
+},
+{
+ title:"Daisy Jones and The Six",
+ author:"Taylor Jenkins Reid",
+ pages:368,
+ read:true,
+ imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1580255154i/40597810.jpg",
+ id:crypto.randomUUID(),
+},
+{
+title:"Once Upon a River",
+author:"Diane Setterfield",
+pages:464,
+read:true,
+imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1526451355i/40130093.jpg",
+id:crypto.randomUUID(),
+},
+{
+title:"Circe",
+author:"Madeline Miller",
+pages:393,
+read:true,
+imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1565909496i/35959740.jpg",
+id:crypto.randomUUID(),
+},
+{
+title:"Listen, Little Man!",
+author:"Wilhelm Reich",
+pages:144,
+read:true,
+imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1388195203i/339387.jpg",
+id:crypto.randomUUID()
+},
+{
+title:"The Prophet",
+author:"Kahlil Gibran",
+pages:90,
+read:true,
+imageURL:"https://m.media-amazon.com/images/I/61aILYFZ5DL._AC_UF894,1000_QL80_.jpg",
+id:crypto.randomUUID(),
+},
+{
+title:"The Kite Runner",
+author:"Khaled Hosseini",
+pages:517,
+read:true,
+imageURL:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203.jpg",
+id:crypto.randomUUID(),
+},
+{
+  title:"Love in the Time of Cholera",
+  author:"Gabriel Garcia Marquez",
+  pages:479,
+  read:true,
+  imageURL:"https://m.media-amazon.com/images/I/81NtAdQ3sUL._AC_UF1000,1000_QL80_.jpg",
+  id:crypto.randomUUID(),
+},
+];
+
+const server = http.createServer((req, res) => {
+
+  
+  if (req.method === "GET" && req.url === "/books") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(books));
+    return;
+  }
+
+  if (req.method === "POST" && req.url === "/books") {
+    let body = "";
+
+    req.on("data", chunk => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const newBook = JSON.parse(body);
+      books.push(newBook);
+
+      res.writeHead(201, {"Content-Type": "application/json"});
+      res.end(JSON.stringify(newBook));
+    });
+    return;
+  }
+
+  const filePath = req.url === "/" ? "./index.html" : `.${req.url}`;
+  const ext = path.extname(filePath);
+
+  const mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg"
+  };
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 - Not Found");
+    } else {
+      res.writeHead(200, { "Content-Type": mimeTypes[ext] || "application/octet-stream"});
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const myLibrary = [];
+//localStorage.removeItem("library");
+
+
+/*function loadLibrary() {
+
+    const stored = localStorage.getItem("library");
+
+    if(!stored) return [];
+
+    return JSON.parse(stored);
+}
+const myLibrary = loadLibrary();
+
+function saveLibrary() {
+    localStorage.setItem("library", JSON.stringify(myLibrary));
+}
 
 
 function Book(title, author, pages, read, imageURL) {
@@ -14,6 +29,7 @@ function Book(title, author, pages, read, imageURL) {
 function addBookToLibrary(title, author, pages, read, imageURL) {
     const newBook = new Book(title, author, pages, read, imageURL);
     myLibrary.push(newBook);
+    saveLibrary();
     return newBook;
 }
 
@@ -26,258 +42,46 @@ Book.prototype.info = function() {
 
     Book.prototype.toggleRead = function() {
         this.read = !this.read;
+        saveLibrary();
     };
 
-addBookToLibrary(
-    "1984", 
-    "George Orwell", 
-    328, 
-    true,
-    "https://cdn.waterstones.com/bookjackets/large/9780/1410/9780141036144.jpg"
-);
 
-addBookToLibrary(
-    "Burial Rights",
-    "Hannah Kent",
-    420,
-    true,
-    "https://m.media-amazon.com/images/I/81yBp-oayWL._AC_UF894,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "The Shadow of the Wind",
-    "Carlos Ruiz Zafón",
-    544,
-    true,
-    "https://m.media-amazon.com/images/I/913DdP7RflL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "Chess Story",
-    "Stefan Zweig",
-    112,
-    true,
-    "https://m.media-amazon.com/images/I/71I6zQJwPnL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "Brave New World",
-    "Aldous Huxley",
-    288,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1575509280i/5129.jpg"
-);
-
-addBookToLibrary (
-    "Lord of Flies",
-    "William Golding",
-    240,
-    true,
-    "https://m.media-amazon.com/images/I/81xVnX8OodL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "His Dark Materials Trilogy",
-    "Philip Pullman",
-    1088,
-    true,
-    "https://prodimage.images-bn.com/pimages/9780307957832_p0_v2_s600x595.jpg"
-);
-
-addBookToLibrary(
-    "Frankenstein",
-    "Mary Shelley",
-    240,
-    true,
-    "https://www.magicmurals.com/media/catalog/product/cache/af1e2a1566fa2dbb552605e8822354b7/a/d/adg-0000001045_1.jpg"
-);
-
-addBookToLibrary(
-    "Confusion",
-    "Stefan Zweig",
-    176,
-    true,
-    "https://m.media-amazon.com/images/I/71+v5U30orL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "When Nietzsche Wept",
-    "Irvin D. Yalom",
-    310,
-    true,
-    "https://m.media-amazon.com/images/I/81z5b4ZHKoL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "Girl, Woman, Other",
-    "Bernardine Evaristo",
-    464,
-    true,
-    "https://m.media-amazon.com/images/I/71TpVXA19ZL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "When the Crawdads Sing",
-    "Delia Owens",
-    384,
-    true,
-    "https://m.media-amazon.com/images/I/81e+mSqZvnL._AC_UF894,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "The Secret History",
-    "Donna Tart",
-    559,
-    true,
-    "https://m.media-amazon.com/images/I/81YhQfeiynL.jpg"
-);
-
-addBookToLibrary(
-    "Love's Executioner and Other Tales of Psychotherapy",
-    "Irvin D. Yalom",
-    304,
-    true,
-    "https://m.media-amazon.com/images/I/81ciP8gyJTL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "Animal Farm",
-    "George Orwell",
-    112,
-    true,
-    "https://m.media-amazon.com/images/I/91Lbhwt5RzL.jpg"  
-);
-
-addBookToLibrary(
-    "Just Above my Head",
-    "James Baldwin",
-    584,
-    true,
-    "https://m.media-amazon.com/images/I/81JUgWD6xWL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "Memoirs of a Geisha",
-    "Arthur Golden",
-    503,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1409595968i/929.jpg"
-);
-
-addBookToLibrary(
-    "The Picture of Dorian Gray",
-    "Oscar Wilde",
-    272,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1546103428i/5297.jpg"
-);
-
-addBookToLibrary(
-    "Invisible Women",
-    "Caroline Criado Perez",
-    272,
-    true,
-    "https://m.media-amazon.com/images/I/61OCni--zZL._AC_UF1000,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "Desert Flower",
-    "Waris Dirie",
-     256,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1439762656i/8745.jpg"
-);
-
-addBookToLibrary(
-    "I'll Give you the Sun",
-    "Jandy Nelson",
-    371,
-    true,
-    "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1608612077l/20820994.jpg"
-);
+if (myLibrary.length === 0) {
+    addBookToLibrary("1984", "George Orwell", 328, true, "https://cdn.waterstones.com/bookjackets/large/9780/1410/9780141036144.jpg");
+    addBookToLibrary("Burial Rights", "Hannah Kent", 420, true, "https://m.media-amazon.com/images/I/81yBp-oayWL._AC_UF894,1000_QL80_.jpg");
+    addBookToLibrary("The Shadow of the Wind", "Carlos Ruiz Zafón", 544, true, "https://m.media-amazon.com/images/I/913DdP7RflL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("Chess Story", "Stefan Zweig", 112, true, "https://m.media-amazon.com/images/I/71I6zQJwPnL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("Brave New World", "Aldous Huxley", 288, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1575509280i/5129.jpg");
+    addBookToLibrary("Lord of Flies", "William Golding", 240, true, "https://m.media-amazon.com/images/I/81xVnX8OodL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("His Dark Materials Trilogy", "Philip Pullman", 1088, true, "https://prodimage.images-bn.com/pimages/9780307957832_p0_v2_s600x595.jpg");
+    addBookToLibrary("Frankenstein", "Mary Shelley", 240, true, "https://www.magicmurals.com/media/catalog/product/cache/af1e2a1566fa2dbb552605e8822354b7/a/d/adg-0000001045_1.jpg");
+    addBookToLibrary("Confusion", "Stefan Zweig", 176, true, "https://m.media-amazon.com/images/I/71+v5U30orL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("When Nietzsche Wept", "Irvin D. Yalom", 310, true, "https://m.media-amazon.com/images/I/81z5b4ZHKoL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("Girl, Woman, Other", "Bernardine Evaristo", 464, true, "https://m.media-amazon.com/images/I/71TpVXA19ZL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("When the Crawdads Sing", "Delia Owens", 384, true, "https://m.media-amazon.com/images/I/81e+mSqZvnL._AC_UF894,1000_QL80_.jpg");
+    addBookToLibrary("The Secret History", "Donna Tart", 559, true, "https://m.media-amazon.com/images/I/81YhQfeiynL.jpg");
+    addBookToLibrary("Love's Executioner and Other Tales of Psychotherapy", "Irvin D. Yalom", 304, true, "https://m.media-amazon.com/images/I/81ciP8gyJTL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("Animal Farm", "George Orwell", 112, true, "https://m.media-amazon.com/images/I/91Lbhwt5RzL.jpg");
+    addBookToLibrary("Just Above my Head", "James Baldwin", 584, true, "https://m.media-amazon.com/images/I/81JUgWD6xWL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("Memoirs of a Geisha", "Arthur Golden", 503, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1409595968i/929.jpg");
+    addBookToLibrary("The Picture of Dorian Gray", "Oscar Wilde", 272, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1546103428i/5297.jpg");
+    addBookToLibrary("Invisible Women", "Caroline Criado Perez", 272, true, "https://m.media-amazon.com/images/I/61OCni--zZL._AC_UF1000,1000_QL80_.jpg");
+    addBookToLibrary("Desert Flower", "Waris Dirie", 256, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1439762656i/8745.jpg");
+    addBookToLibrary("I'll Give you the Sun", "Jandy Nelson", 371, true, "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1608612077l/20820994.jpg");
+    addBookToLibrary("The Song of Achilles", "Madeline Miller", 408, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1357177533i/13623848.jpg");
+    addBookToLibrary("The Seven Husbands of Evelyn Hugo", "Taylor Jenkins Reid", 389, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1664458703i/32620332.jpg");
+    addBookToLibrary("The Seven Deaths of Evelyn Hardcastle", "Stuart Turton", 432, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1506896221i/36337550.jpg");
+    addBookToLibrary("Daisy Jones and The Six", "Taylor Jenkins Reid", 368, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1580255154i/40597810.jpg");
+    addBookToLibrary("Once Upon a River", "Diane Setterfield", 464, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1526451355i/40130093.jpg");
+    addBookToLibrary("Circe", "Madeline Miller", 393, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1565909496i/35959740.jpg");
+    addBookToLibrary("Listen, Little Man!", "Wilhelm Reich", 144, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1388195203i/339387.jpg");
+    addBookToLibrary("The Prophet", "Kahlil Gibran", 90, true, "https://m.media-amazon.com/images/I/61aILYFZ5DL._AC_UF894,1000_QL80_.jpg");
+    addBookToLibrary("The Kite Runner", "Khaled Hosseini", 517, true, "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203.jpg");
+    addBookToLibrary("Love in the Time of Cholera", "Gabriel Garcia Marquez", 479, true, "https://m.media-amazon.com/images/I/81NtAdQ3sUL._AC_UF1000,1000_QL80_.jpg");
+} */
 
 
-addBookToLibrary(
-    "The Song of Achilles",
-     "Madeline Miller",
-     408,
-     true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1357177533i/13623848.jpg"
-);
-
-addBookToLibrary(
-    "The Seven Husbands of Evelyn Hugo",
-    "Taylor Jenkins Reid",
-    389,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1664458703i/32620332.jpg"
-);
-
-addBookToLibrary(
-    "The Seven Deaths of Evelyn Hardcastle",
-    "Stuart Turton",
-    432,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1506896221i/36337550.jpg"
-);
-
-addBookToLibrary(
-    "Daisy Jones and The Six",
-    "Taylor Jenkins Reid",
-    368,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1580255154i/40597810.jpg"
-);
-
-addBookToLibrary(
-    "Once Upon a River",
-    "Diane Setterfield",
-    464,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1526451355i/40130093.jpg"
-);
-
-addBookToLibrary(
-    "Circe",
-    "Madeline Miller",
-    393,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1565909496i/35959740.jpg"
-);
-
-addBookToLibrary(
-    "Listen, Little Man!",
-    "Wilhelm Reich",
-    144,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1388195203i/339387.jpg"
-);
-
-addBookToLibrary(
-    "The Prophet",
-    "Kahlil Gibran",
-    90, 
-    true,
-    "https://m.media-amazon.com/images/I/61aILYFZ5DL._AC_UF894,1000_QL80_.jpg"
-);
-
-addBookToLibrary(
-    "The Kite Runner",
-    "Khaled Hosseini",
-    517,
-    true,
-    "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1579036753i/77203.jpg"
-);
-
-addBookToLibrary(
-     "Love in the Time of Cholera",
-    "Gabriel Garcia Marquez",
-    479,
-    true,
-   "https://m.media-amazon.com/images/I/81NtAdQ3sUL._AC_UF1000,1000_QL80_.jpg"
-);
-
-myLibrary.forEach(book => createBookCard(book));
+//myLibrary.forEach(book => createBookCard(book));
 
 function createBookCard(book) {
     const card = document.createElement("div");
@@ -352,6 +156,7 @@ card.appendChild(buttonGroup);
     const index = myLibrary.findIndex(b => b.id === book.id);
     if (index !== -1){
         myLibrary.splice(index, 1);
+        saveLibrary();
         card.remove();
         showToast("Book removed!")
     }
@@ -376,23 +181,58 @@ newBookBtn.addEventListener("click", () => {
     }
 });
 
-bookForm .addEventListener("submit", function(e) {
+/*function bookExists(title) {
+    return myLibrary.some(book => book.title === title);
+} */
+
+bookForm.addEventListener("submit", function(e) {
     e.preventDefault();
+   
+    const book = {
+     title: document.getElementById("title").value.trim(),
+     author: document.getElementById("author").value.trim(),
+     pages:  parseInt(document.getElementById("pages").value),
+     read: document.getElementById("read").checked,
+     imageURL: document.getElementById("imageURL").value.trim(),
+      };
 
-    const title = document.getElementById("title").value.trim();
-    const author = document.getElementById("author").value.trim();
-    const pages = parseInt(document.getElementById("pages").value);
-    const read = document.getElementById("read").checked;
-    const imageURL = document.getElementById("imageURL").value.trim();
+       fetch("/books", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(book)
+       })
+       .then(response => response.json())
+       .then(() => {
+        bookForm.reset();
+        loadBooks();
+        showToast("Book added!");
+       })
+       .catch(err => {
+        console.log(err);
+        showToast("Failed to add book");
+       });
+    });
 
-    const newBook = addBookToLibrary(title, author, pages, read, imageURL);
+    function loadBooks() {
+      fetch("/books")
+    .then(res => res.json())
+    .then(books => {
+      const container = document.getElementById("library");
+      container.innerHTML = "";
 
-    createBookCard(newBook);
+      books.forEach(book => {
+        createBookCard(book);
+      });
+    })
+    .catch(err => {
+        console.log("Failed to load books:", err);
+    }); 
+}
 
-    bookForm.reset();
-    bookForm.style.display = "none";
-    showToast("Book added successfully!");
-});
+loadBooks();
+    
 
 function showToast(message) {
     const toast = document.getElementById("toast");
@@ -432,3 +272,19 @@ commentForm.addEventListener("submit", (e) => {
 
     commentForm.reset();
 });
+
+/*fetch("/books") 
+.then(response => response.json())
+.then(books => {
+   
+    books.forEach(book => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+        <h3>${book.title}</h3>
+        <p>${book.author}</p>`;
+        container.appendChild(card);
+    });
+}) 
+.catch(err => console.error("Fetch error:", err)); */
