e83ad736cffcb26a2addf78486d3d99cf94dd9f2
Christina Trochidou <xristinatrox@yahoo.gr>
2026-01-20 11:58:20 +0200

    Fix root route for deployment

--- index.js
+++ index.js
@@ -300,7 +300,13 @@ app.post("/books", (req, res) => {
       saveBooks();
       res.json(book);
     });
+    
+    
+
+    app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "index.html"));
+    });
        
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running on http://localhost:3000");
 });
